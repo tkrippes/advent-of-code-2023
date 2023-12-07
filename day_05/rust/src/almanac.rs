c66540a78f8fc8bb04c174f0bca8419abec71b77
Tom Krippes <tkrippes@live.de>
2023-12-07 10:57:36 +0100

    Created skeleton implementation for part 1
    - added desired output for part 1 test

---
+++ day_05/rust/src/almanac.rs
@@ -0,0 +1,106 @@
+struct Ranges {
+    destination_start_range: u64,
+    source_start_range: u64,
+    range_lengths: u64,
+}
+
+impl Ranges {
+    fn try_build(input: &str) -> Option<Self> {
+        todo!()
+    }
+}
+
+struct Map {
+    ranges: Vec<Ranges>,
+}
+
+impl Map {
+    fn try_build(input: Vec<&str>) -> Option<Self> {
+        todo!()
+    }
+
+    fn get_destination_from_source(&self, source: u64) -> u64 {
+        todo!()
+    }
+}
+
+struct Maps {
+    seed_to_soil_map: Map,
+    soil_to_fertilizer_map: Map,
+    fertilizer_to_water_map: Map,
+    water_to_light_map: Map,
+    light_to_temperature_map: Map,
+    temperature_to_humidity_map: Map,
+    humidity_to_location_map: Map,
+}
+
+impl Maps {
+    fn try_build(input: Vec<&str>) -> Option<Self> {
+        todo!()
+    }
+
+    fn get_location_from_seed(&self, seed: u64) -> u64 {
+        let soil = self.seed_to_soil_map.get_destination_from_source(seed);
+
+        let fertilizer = self
+            .soil_to_fertilizer_map
+            .get_destination_from_source(soil);
+
+        let water = self
+            .fertilizer_to_water_map
+            .get_destination_from_source(fertilizer);
+
+        let light = self.water_to_light_map.get_destination_from_source(water);
+
+        let temperature = self
+            .light_to_temperature_map
+            .get_destination_from_source(light);
+
+        let humidity = self
+            .temperature_to_humidity_map
+            .get_destination_from_source(temperature);
+
+        let location = self
+            .humidity_to_location_map
+            .get_destination_from_source(humidity);
+
+        location
+    }
+}
+
+pub struct Almanac {
+    seeds: Vec<u64>,
+    maps: Maps,
+}
+
+impl Almanac {
+    pub fn try_build(input: Vec<&str>) -> Option<Self> {
+        let mut seeds: Option<Vec<u64>> = None;
+
+        if let Some(seeds_input) = input.first() {
+            seeds = Almanac::try_build_seeds(seeds_input);
+        }
+
+        let maps = Maps::try_build(input[2..].to_vec());
+
+        if let (Some(seeds), Some(maps)) = (seeds, maps) {
+            Some(Almanac { seeds, maps })
+        } else {
+            None
+        }
+    }
+
+    fn try_build_seeds(input: &str) -> Option<Vec<u64>> {
+        todo!()
+    }
+
+    pub fn get_locations_from_seeds(&self) -> Vec<u64> {
+        let mut locations = Vec::new();
+
+        for seed in &self.seeds {
+            locations.push(self.maps.get_location_from_seed(*seed));
+        }
+
+        locations
+    }
+}
--- day_05/rust/src/lib.rs
+++ day_05/rust/src/lib.rs
@@ -1,9 +1,43 @@
+mod almanac;
+
+use almanac::Almanac;
+use std::fs;
+
 pub fn part_1(file_name: &str) -> u64 {
-    todo!()
+    let almanac = try_get_almanac(file_name);
+
+    if let Some(almanac) = almanac {
+        get_lowest_location_of_initial_seeds(&almanac)
+    } else {
+        println!("Failed to get almanac");
+        0
+    }
 }
 
 pub fn part_2(file_name: &str) -> u64 {
     todo!()
+}
+
+fn try_get_almanac(file_name: &str) -> Option<Almanac> {
+    let file_content =
+        fs::read_to_string(file_name).expect("input file should be located in input folder");
+    let file_lines: Vec<&str> = file_content
+        .split('\n')
+        .map(|file_line| file_line.trim())
+        .collect();
+
+    Almanac::try_build(file_lines)
+}
+
+fn get_lowest_location_of_initial_seeds(almanac: &Almanac) -> u64 {
+    let locations = almanac.get_locations_from_seeds();
+
+    if let Some(minimum_location) = locations.iter().min() {
+        *minimum_location
+    } else {
+        println!("Failed to get minimum location");
+        0
+    }
 }
 
 #[cfg(test)]
@@ -13,7 +47,7 @@ mod tests {
     #[test]
     fn test_input_part_1() {
         let result = part_1("../input/test_input.txt");
-        assert_eq!(result, 0);
+        assert_eq!(result, 35);
     }
 
     #[test]
