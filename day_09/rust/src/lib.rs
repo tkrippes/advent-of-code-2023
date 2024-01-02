dda23e04a330e8e9956041203a0b878960336ba2
tkrippes <tkrippes@live.de>
2024-01-02 20:50:50 +0100

    Added skeleton for day 9
    - added instructions for part 1
    - added inputs
    - added test input result for part 1

---
+++ day_09/rust/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn part_1(file_name: &str) -> u32 {
+    todo!()
+}
+
+pub fn part_2(file_name: &str) -> u32 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_input_part_1() {
+        let result = part_1("../input/test_input.txt");
+        assert_eq!(result, 114);
+    }
+
+    #[test]
+    fn test_input_part_2() {
+        let result = part_2("../input/test_input.txt");
+        assert_eq!(result, 0);
+    }
+}
