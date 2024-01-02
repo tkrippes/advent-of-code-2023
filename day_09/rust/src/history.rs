9dcb4a20c87c38fa8821cf08e707a6f6898bafad
tkrippes <tkrippes@live.de>
2024-01-02 22:42:46 +0100

    Refactoring
    - Using VecDeque for front insertion improvement
    - proper option handling instead of unwrap
    - method renaming

--- day_09/rust/src/history.rs
+++ day_09/rust/src/history.rs
@@ -1,14 +1,16 @@
+use std::collections::VecDeque;
+
 pub struct History {
-    values: Vec<i32>,
+    values: VecDeque<i32>,
 }
 
 impl History {
     pub fn try_build(input: &str) -> Option<Self> {
-        let mut values = Vec::new();
+        let mut values = VecDeque::new();
 
         for value in input.split_whitespace() {
             match value.parse::<i32>() {
-                Ok(value) => values.push(value),
+                Ok(value) => values.push_back(value),
                 Err(value_parsing_error) => {
                     println!("Cannot build history, {}", value_parsing_error);
                     return None;
@@ -23,69 +25,86 @@ impl History {
         let mut sequences = vec![self.values.clone()];
 
         while Self::is_there_a_non_zero_value_in_last_sequence(&sequences) {
-            Self::fill_sequences(&mut sequences);
+            Self::add_next_sequence(&mut sequences);
         }
 
-        Self::fill_back_placeholders(&mut sequences);
+        Self::fill_next_placeholders(&mut sequences);
 
-        *sequences.first().unwrap().last().unwrap()
+        match sequences.first() {
+            Some(first_sequence) => match first_sequence.back() {
+                Some(next_value) => *next_value,
+                None => 0,
+            },
+            None => 0,
+        }
     }
 
     pub fn get_prediction_of_previous_value(&self) -> i32 {
         let mut sequences = vec![self.values.clone()];
 
         while Self::is_there_a_non_zero_value_in_last_sequence(&sequences) {
-            Self::fill_sequences(&mut sequences);
+            Self::add_next_sequence(&mut sequences);
         }
 
-        Self::fill_front_placeholders(&mut sequences);
+        Self::fill_previous_placeholders(&mut sequences);
 
-        *sequences.first().unwrap().first().unwrap()
+        match sequences.first() {
+            Some(first_sequence) => match first_sequence.front() {
+                Some(previous_value) => *previous_value,
+                None => 0,
+            },
+            None => 0,
+        }
     }
 
-    fn is_there_a_non_zero_value_in_last_sequence(sequences: &[Vec<i32>]) -> bool {
+    fn is_there_a_non_zero_value_in_last_sequence(sequences: &[VecDeque<i32>]) -> bool {
         match sequences.last() {
             Some(last_sequence) => last_sequence.iter().any(|value| *value != 0),
             None => false,
         }
     }
 
-    fn fill_sequences(sequences: &mut Vec<Vec<i32>>) {
-        let mut next_sequence = Vec::new();
+    fn add_next_sequence(sequences: &mut Vec<VecDeque<i32>>) {
+        if let Some(last_sequence) = sequences.last() {
+            let mut next_sequence = VecDeque::new();
 
-        for index in 0..sequences.last().unwrap().len() - 1 {
-            next_sequence
-                .push(sequences.last().unwrap()[index + 1] - sequences.last().unwrap()[index]);
+            for index in 0..last_sequence.len() - 1 {
+                next_sequence.push_back(last_sequence[index + 1] - last_sequence[index]);
+            }
+
+            sequences.push(next_sequence);
         }
-
-        sequences.push(next_sequence);
     }
 
-    fn fill_back_placeholders(sequences: &mut Vec<Vec<i32>>) {
-        sequences.last_mut().unwrap().push(0);
+    fn fill_next_placeholders(sequences: &mut Vec<VecDeque<i32>>) {
+        if let Some(last_sequence) = sequences.last_mut() {
+            last_sequence.push_back(0);
+        }
 
         for index in (1..=sequences.len() - 1).rev() {
-            let last_previous_value = *sequences.get(index - 1).unwrap().last().unwrap();
-            let last_current_value = *sequences.get(index).unwrap().last().unwrap();
+            let last_previous_value = *sequences.get(index - 1).unwrap().back().unwrap();
+            let last_current_value = *sequences.get(index).unwrap().back().unwrap();
 
             sequences
                 .get_mut(index - 1)
                 .unwrap()
-                .push(last_previous_value + last_current_value)
+                .push_back(last_previous_value + last_current_value)
         }
     }
 
-    fn fill_front_placeholders(sequences: &mut Vec<Vec<i32>>) {
-        sequences.last_mut().unwrap().insert(0, 0);
+    fn fill_previous_placeholders(sequences: &mut Vec<VecDeque<i32>>) {
+        if let Some(last_sequence) = sequences.last_mut() {
+            last_sequence.push_front(0);
+        }
 
         for index in (1..=sequences.len() - 1).rev() {
-            let first_previous_value = *sequences.get(index - 1).unwrap().first().unwrap();
-            let first_current_value = *sequences.get(index).unwrap().first().unwrap();
+            let first_previous_value = *sequences.get(index - 1).unwrap().front().unwrap();
+            let first_current_value = *sequences.get(index).unwrap().front().unwrap();
 
             sequences
                 .get_mut(index - 1)
                 .unwrap()
-                .insert(0, first_previous_value - first_current_value)
+                .push_front(first_previous_value - first_current_value)
         }
     }
 }
